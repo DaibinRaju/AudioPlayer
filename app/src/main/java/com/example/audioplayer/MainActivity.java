a7d53fd798bc0467d67e61060402fa17ef140651
DaibinRaju <daibinraju3200@gmail.com>
2020-01-06 11:45:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/audioplayer/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.audioplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button play=(Button) findViewById(R.id.play);
+        Button pause=(Button) findViewById(R.id.pause);
+        Button stop =(Button) findViewById(R.id.stop);
+
+        final MediaPlayer mediaPlayer= MediaPlayer.create(this,R.raw.song);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.reset();
+            }
+        });
+    }
+}
